272380b62f8647ec47a90b493d2ef54302837f7c
kumariishruti0511-ai <kumariishruti0511@gmail.com>
2025-11-22 23:53:52 +0530

    logic for sum by user

--- java_programming/src/java_programming/Sum.java
+++ java_programming/src/java_programming/Sum.java
@@ -1,11 +1,21 @@
 package java_programming;
 
+import java.util.Scanner;
+
 public class Sum {
 
+	public void sumNumber(int a, int b) {
+		int s;
+		s=a+b;
+		System.out.println("Sum of two number= "+s);
+	}
 	public static void main(String[] args) {
-		int a=8,b=22,s=a+b;
-		System.out.println("sum="+s);
-
+		Sum obj=new Sum();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		obj.sumNumber(a, b);
 	}
 
 }
